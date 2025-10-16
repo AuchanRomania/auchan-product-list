f3cd49fa43679f836de44131f2581f447e7c6cc5
rusude <ovidiugabriel999@gmail.com>
2025-10-16 16:02:11 +0300

    Modified the logic of price value based on the item availability

--- react/Price.tsx
+++ react/Price.tsx
@@ -37,7 +37,7 @@ const Price: React.FC<PriceProps> = ({
         handles.productPriceContainer
       } ${parseTextAlign(textAlign)}`}
     >
-      {item.listPrice && item.listPrice !== item.sellingPrice && showListPrice && (
+      {item.listPrice && item.listPrice !== item.sellingPrice && showListPrice && item.availability === 'available' && (
         <div
           id={`list-price-${item.id}`}
           className={`${handles.productPriceCurrency}  ${applyModifiers(
@@ -63,7 +63,9 @@ const Price: React.FC<PriceProps> = ({
         <FormattedPrice
           value={
             item.sellingPrice != null
-              ? (item.sellingPrice * item.quantity) / 100
+              ? item.sellingPrice >= 0 && item.availability === 'available'
+                ? (item.sellingPrice * (item.unitMultiplier ?? 1) * item.quantity) / 100
+                : null
               : item.sellingPrice
           }
         />
